20c71e99af5ec8141edb85b0bc072caeb17042c2
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-13 16:09:00 +0800

    聊天界面的架构

---
+++ src/components/chat/bubblepanel/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class BubblePanel extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
-export default class Login extends Component {
-  render(){
-    return(
-      <div>聊天界面</div>
-    )
-  }
+import SlideBar from './slidebar';
+import SessionList from './sessionlist';
+import BubblePanel from './bubblepanel';
+export default class Chat extends Component {
+
+    render() {
+        console.log(this.props);
+        return (
+            <div className="chat">
+                <div>
+                    <SlideBar />
+                    <SessionList />
+                    <BubblePanel />
+                </div>
+
+            </div>
+        );
+    }
 }
