33c9bae72af530b1aafb7610eddaf24a9a3cc30a
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-13 23:40:37 +0800

    聊天界面基本组成

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-
 import SlideBar from './slidebar';
 import SessionList from './sessionlist';
 import BubblePanel from './bubblepanel';
+import './index.css';
+
 export default class Chat extends Component {
 
     render() {
-        console.log(this.props);
         return (
-            <div className="chat">
+            <div className="ctn-chat">
                 <div>
                     <SlideBar />
                     <SessionList />
                     <BubblePanel />
                 </div>
-
             </div>
         );
     }
