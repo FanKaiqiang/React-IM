f00c976c8cf3975fb08455bf543cacb0dbe1998e
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-14 17:47:29 +0800

    chatid存在才会渲染消息页面

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -7,13 +7,13 @@ import './index.css';
 export default class Chat extends Component {
 
     render() {
-        let {params} = this.props;
+        let { params } = this.props;
         return (
             <div className="ctn-chat">
                 <div>
                     <SlideBar />
-                    <SessionList chatType = {params.chatType} chatId = {params.chatId}/>
-                    <BubblePanel />
+                    <SessionList chatType={params.chatType} chatId={params.chatId} />
+                    {params.chatId ? <BubblePanel chatType={params.chatType} chatId={params.chatId} /> : null}
                 </div>
             </div>
         );
