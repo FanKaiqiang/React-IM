12f16c4b7886a2d2c8a68594d6dee794c6a64e9a
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-15 20:47:42 +0800

    在Redux添加setRosters的Action

--- src/data/actions/actiontypes.js
+++ src/data/actions/actiontypes.js
@@ -10,6 +10,7 @@ export function createAction(type, ...actionArgs) {//创建action的函数，返
 
 export const REG_STATE_CHANGE = 'reg_state_change';
 export const SET_CURRENT_SESSION = 'set_currrnet_session';
+export const SET_ROSTERS = 'set_rosters';
 export const SEND_TEXT_MSG = 'send_text_msg';
 export const GET_MSGS = 'get_msgs';
 export const CHANGE_MSG_STATUS = 'change_msg_status';
