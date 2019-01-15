c5d06d606b01b3a1581983d0cd5cc56c64afc33e
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-15 11:34:55 +0800

    发送消息的Action的发起

--- src/components/chat/bubblepanel/index.js
+++ src/components/chat/bubblepanel/index.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { sendTextMessage } from '@data/actions/message';
+import { sendTextMessage } from '@data/actions/message';
 import './index.css';
 
 @connect(
     (state) => ({
         currentSession: state.session.current,
-        // msglist: state.message.msglist
+        msglist: state.message.msglist
     }),
     {
-        // sendTextMessage
+        sendTextMessage
     }
 )
 export default class BubblePanel extends Component {
+    sendTextMessage = () => {
+        let {sendTextMessage, currentSession, chatType} = this.props;
+        sendTextMessage(currentSession.name, this.refs.msginput.value, chatType);
+        this.refs.msginput.value = '';
+    }
+
+    getMsgs =  () => {
+        let {msglist, currentSession} = this.props;
+        return msglist[currentSession.name] || [];
+    }
     render() {
         let { currentSession } = this.props;
+        let msgs = this.getMsgs();
         return (
             <div className="ctn-bubblepanel">
                 <div className="title">
                     {currentSession ? currentSession.name : ''}
                 </div>
                 <div className="ctn-msglist">
-                    {/* {msgs.map((msg) => {
+                    {msgs.map((msg) => {
                         return <div key={msg.id}>{msg.value}
                         </div>
-                    })} */}
+                    })}
                 </div>
                 <div className="ctn-msg-sender">
                     <textarea
                         ref="msginput"
                         placeholder="输入消息"
                     />
-                    <button className="button"
-                    // onClick = {this.sendTextMessage}
-                    >发送</button>
+                    <button className="button" onClick = {this.sendTextMessage}>发送</button>
                 </div>
             </div>
         );
