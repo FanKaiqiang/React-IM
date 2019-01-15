42d52e581d426aa7f9a87cfa20ad0825fbc9a36c
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-15 14:45:33 +0800

    消息项中添加头像与发送人

--- src/components/chat/bubblepanel/index.js
+++ src/components/chat/bubblepanel/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Avator from '@component/common/avator';
+import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { sendTextMessage } from '@data/actions/message';
 import './index.css';
@@ -14,13 +16,16 @@ import './index.css';
 )
 export default class BubblePanel extends Component {
     sendTextMessage = () => {
-        let {sendTextMessage, currentSession, chatType} = this.props;
+        let { sendTextMessage, currentSession, chatType } = this.props;
         sendTextMessage(currentSession.name, this.refs.msginput.value, chatType);
         this.refs.msginput.value = '';
     }
 
-    getMsgs =  () => {
-        let {msglist, currentSession} = this.props;
+    getMsgs = () => {
+        let { msglist, currentSession } = this.props;
+        if (!currentSession) {
+            return []
+        }
         return msglist[currentSession.name] || [];
     }
     render() {
@@ -32,23 +37,53 @@ export default class BubblePanel extends Component {
                     {currentSession ? currentSession.name : ''}
                 </div>
                 <div className="ctn-msglist">
-                    {msgs.map((msg) => {
-                        return <div key={msg.id}>{msg.value}
-                        </div>
-                    })}
+                    <div className="ctn-msglist-inner">
+                        {msgs.map((msg) => {
+                            return <BubbleItem key={msg.id} msg={msg} />
+                        })}
+                    </div>
                 </div>
                 <div className="ctn-msg-sender">
                     <textarea
                         ref="msginput"
                         placeholder="输入消息"
                     />
-                    <button className="button" onClick = {this.sendTextMessage}>发送</button>
+                    <button className="button" onClick={this.sendTextMessage}>发送</button>
                 </div>
             </div>
         );
     }
 }
 
-// class BubbleItem extends Component {
+class BubbleItem extends Component {
+    render() {
+        let { msg } = this.props;
+        let fromMe = true; //true 表示我发出去， to 表示我收到的
 
-// }
+        let messageItemClassName = classnames({
+            'message-item': true,
+            'you': !fromMe,
+            'me': fromMe
+        });
+        return (
+            <div className={messageItemClassName}>
+                {!fromMe ? <div className="avator-outer">
+                    <Avator />
+                </div> : null}
+                <div className="message-item-inner">
+                    <div className="name">
+                        {fromMe ? msg.from : msg.to}
+                    </div>
+                    <div className="message-text">
+                        {msg.value}
+                    </div>
+                </div>
+
+                {fromMe ? <div className="avator-outer">
+                    <Avator />
+                </div> : null}
+            </div>
+        )
+    }
+
+}
